a1bce5f7e37ceb3df32fa02f96cb0f951d889729
jkaramon <karamon@softcamp.cz>
2013-03-22 13:49:51 +0100

    2013-03-22 13:49:51 +0100

--- _plugins/news_img.rb
+++ _plugins/news_img.rb
@@ -21,6 +21,28 @@ module Jekyll
 
   end
 
+  class NewsImgBigTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+      @tokens = tokens
+    end
+
+    def render(context)
+      dt = context['page']['date'].strftime("%Y-%m-%d")
+      src = "/images/news/#{dt}/#{@text}"
+      <<-eos
+        <span class="shadow">
+          <a href="#{src}" class="popup">
+            <img src="#{src}" class="shadow big" />
+          </a>
+        </span>
+      eos
+    end
+
+  end
+
+
   class NewsImgUrlTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
@@ -37,6 +59,8 @@ module Jekyll
 
 
 end
+
+Liquid::Template.register_tag('news_img_big', Jekyll::NewsImgBigTag)
 
 Liquid::Template.register_tag('news_img', Jekyll::NewsImgTag)
 
